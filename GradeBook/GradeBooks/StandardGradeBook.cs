b82663e972fb0f1266d6bdd7d3b320d0b28c4b35
Juan Pablo <juamportiz@gmail.com>
2019-07-27 21:11:03 -0300

    Changes

--- GradeBook/GradeBooks/StandardGradeBook.cs
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -4,7 +4,11 @@ using System.Text;
 
 namespace GradeBook.GradeBooks
 {
-    class StandardGradeBook
+    public class StandardGradeBook : BaseGradeBook
     {
+        public StandardGradeBook(string name) : base(name)
+        {
+            Type = Enums.GradeBookType.Standard;
+        }
     }
 }
